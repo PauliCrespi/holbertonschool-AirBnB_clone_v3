f2d358e569e923a3525aad8714cc1d6ee7ba8816
EugeniaMatto <eugematto26@gmail.com>
2022-08-10 01:07:34 -0700

    AUTHORS

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.place import place_amenity
+from models.base_model import BaseModel
 
-class Amenity(BaseModel, Base):
-    """class Amenity """
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+
+class Amenity(BaseModel):
+    name = ""
