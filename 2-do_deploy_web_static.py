d49773fff5a115bfe749a52dafa1a17c1078907b
root <4377@holbertonschool.com>
2022-08-25 08:13:51 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,18 +9,17 @@ env.hosts = ['web-01.paulinacrespihs.tech', 'web-02.paulinacrespihs.tech']
 
 def do_deploy(archive_path):
     """distributes archive"""
-    if archive_path is None:
+    if exists(archive_path) is False:
         return False
     fname = archive_path.split("/")[-1]
     fnnoext = fname.split(".")[0]
     folder = "/data/web_static/releases/"
-    try:
-        put(archive_path, '/tmp')
-        run('mkdir -p {}{}'.format(folder, fnnoext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(fname, folder, fnnoext))
-        run('rm /tmp/{}'.format(fname))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(folder, fnnoext))
-        run('rm -rf {}{}/web_static'.format(folder, fnnoext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(folder, fnnoext))
-        return True
+    put(archive_path, '/tmp')
+    run('mkdir -p {}{}'.format(folder, fnnoext))
+    run('tar -xzf /tmp/{} -C {}{}/'.format(fname, folder, fnnoext))
+    run('rm /tmp/{}'.format(fname))
+    run('mv {0}{1}/web_static/* {0}{1}/'.format(folder, fnnoext))
+    run('rm -rf {}{}/web_static'.format(folder, fnnoext))
+    run('rm -rf /data/web_static/current')
+    run('ln -s {}{}/ /data/web_static/current'.format(folder, fnnoext))
+    return True
