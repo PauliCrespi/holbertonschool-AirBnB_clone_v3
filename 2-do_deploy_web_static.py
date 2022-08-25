539b775b63762cd889ee44c3720d3a380256f4aa
root <4377@holbertonschool.com>
2022-08-25 08:00:21 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # task2
-from fabric.api import local
-from datetime import datetime
-from os.path import isdir
+
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['web-01.paulinacrespihs.tech', 'web-02.paulinacrespihs.tech']
 
 
 def do_deploy(archive_path):
@@ -17,10 +19,8 @@ def do_deploy(archive_path):
         run('mkdir -p {}{}'.format(folder, fnnoext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(fname, folder, fnnoext))
         run('rm /tmp/{}'.format(fname))
-        run('mv {}{}/web_static/* {}{}/'.format(folder, fnnoext, folder, fnnoext))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(folder, fnnoext))
         run('rm -rf {}{}/web_static'.format(folder, fnnoext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(folder, fnnoext))
         return True
-    except:
-        return False
