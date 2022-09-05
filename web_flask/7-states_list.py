1354486f9c0a565aa0ffcf089d6bb7d2ed8371dd
root <4377@holbertonschool.com>
2022-09-05 10:23:08 -0700

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,9 @@
 
 from flask import Flask
 from flask import render_template
+from models import storage
+from models.state import State
+
 app = Flask(__name__)
 
 
@@ -13,10 +16,12 @@ def states_list():
     states = sorted(states, key=lambda k: k.name)
     return render_template("7-states_list.html", states=states)
 
+
 @app.teardown_appcontext
-def teardown_app():
+def teardown_app(self):
     """bye"""
     storage.close()
 
+
 if __name__ == '__main__':
-        app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
