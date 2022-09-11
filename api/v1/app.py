c9b6616fd94373abbbb8bca5177eaf34eb12e3d4
root <4377@holbertonschool.com>
2022-09-11 22:49:19 +0000

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,12 @@
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from flask_cors import CORS
 from os import getenv
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
